6074efd83ed49a9b4272a1d8036826d607a1e5ce
syed_vashid <akshan09061999@gmail.com>
2025-03-25 12:32:02 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+from networksecurity.components.data_validation import DataValidation
 import sys
 
 if __name__ == "__main__":
@@ -13,6 +14,16 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed")
         print(dataingestionartifact)
+        data_validation_config =DataValidationConfig(trainingpipelineconfig)
+        data_validation =DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact =data_validation.initiate_data_validation()
+        logging.info("Data validation completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -1,5 +1,14 @@
 from dataclasses import dataclass
 @dataclass
 class DataIngestionArtifacts:
-    trained_file_path: str
-    test_file_path: str
+    trained_file_path:str
+    test_file_path:str
+
+@dataclass
+class DataValidationArtifacts:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
